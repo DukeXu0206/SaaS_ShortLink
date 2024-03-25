f62385c01fe7774ddb4a546ac2a9e8c44c25ab84
Yitian Xu <2733383X@student.gla.ac.uk>
2024-03-25 15:47:23 +0000

    First commit

---
+++ admin/src/main/java/com/nageoffer/shortlink/admin/service/UserService.java
@@ -0,0 +1,10 @@
+package com.nageoffer.shortlink.admin.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.nageoffer.shortlink.admin.dao.entity.UserDO;
+
+/**
+ * 用户接口层
+ */
+public interface UserService extends IService<UserDO> {
+}
